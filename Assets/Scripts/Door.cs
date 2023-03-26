2a4a226426f556923633d71453e0570a3218f329
Ty Pederson <ty23117@gmail.com>
2023-03-26 15:38:19 -0500

    door, door lever, and changed starting point

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    private Vector3 origPos;
+    public bool poweredOn;
+    public float moveDist = 8f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (poweredOn && (origPos.y - transform.position.y) <= moveDist)
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+
+    public void resetPos()
+    {
+        transform.position = origPos;
+        poweredOn = false;
+    }
+
+    public void togglePower()
+    {
+        poweredOn = !poweredOn;
+    }
+}
