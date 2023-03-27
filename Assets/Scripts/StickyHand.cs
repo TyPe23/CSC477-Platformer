d48cc9b105a85908bb84c9e481d4eee115365f31
Damien <ajs074@email.latech.edu>
2023-03-26 19:43:09 -0500

    Created StickyHand sprite and script began scripting

---
+++ Assets/Scripts/StickyHand.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StickyHand")]
+
+public class StickyHand : Powerup
+{
+    public GameObject clickedObj;
+
+
+    // public LayerMask grappableLayer;
+    // This is commented out for if we want a specific layer for walls/objects that can be grappled.
+
+    public override void Apply(GameObject target)
+    {
+        Debug.Log("StickyHand applied");
+        // 
+    }
+
+    void Update()
+    {
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        RaycastHit2D hitData = Physics2D.Raycast(new Vector2(worldPos.x, worldPos.y), Vector2.zero, 0); // add grappableLayer at end here if wanting to use the LayerMask.
+
+        if (hitData && Input.GetMouseButtonDown(0)) // Upon click if an object was clicked, assign it
+        {
+            clickedObj = hitData.transform.gameObject;
+        }
+
+        // Once click position is found we need to get the player from point A (current location) to point B (where the click was made).
+
+
+
+
+
+
+        // Want this to physically draw a ray in play mode to whatever is clicked to visualize where player will be sent by the grapple.
+        Debug.DrawRay(GameObject.FindGameObjectsWithTag("Player")[0].transform.position, worldPos, Color.green);
+    }
+}
